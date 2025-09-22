8ebebd634b80942fa0cef0f44836645a9f9b9bfc
ANDREOLI ANDREA <aandreol@peda.pun.univ-poitiers.fr>
2025-09-22 17:39:38 +0200

    Create NumberOfCharacters component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CharactersList from './components/CharactersList'
+import NumberOfCharacters from './components/NumberOfCharacters'
+import characters from './data/characters.json'
 
 function App() {
   return (
     <div>
       <h1>Personnages Marvel</h1>
+      <NumberOfCharacters characters={characters} />
       <CharactersList />
     </div>
   );
