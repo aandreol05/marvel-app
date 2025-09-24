939f38763f47359c4770041ef2ba86615f935868
ANDREOLI ANDREA <aandreol@peda.pun.univ-poitiers.fr>
2025-09-24 17:19:16 +0200

    Create pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import CharactersList from './components/CharactersList'
-import NumberOfCharacters from './components/NumberOfCharacters'
-import characters from './data/characters.json'
+// import CharactersList from './components/CharactersList'
+// import NumberOfCharacters from './components/NumberOfCharacters'
+// import characters from './data/characters.json'
+import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
+import CharactersPage from './pages/CharactersPage'
 
 function App() {
   return (
     <div>
+      <CharactersPage />
+      {/* 
+      <ContactPage />
+      <AboutPage />
       <h1>Personnages Marvel</h1>
       <NumberOfCharacters characters={characters} />
       <CharactersList characters={characters} />
+      */}
     </div>
   );
 }
---
+++ src/pages/CharactersPage.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import CharactersList from '../components/CharactersList'
+import NumberOfCharacters from '../components/NumberOfCharacters'
+import characters from '../data/characters.json'
+
+function CharactersPage() {
+  useEffect(() => {
+    document.title = 'Marvel App';
+  }, []);
+
+  // Prendre seulement les 4 premiers personnages
+  const firstFourCharacters = characters.slice(0, 4);
+
+  return (
+    <div>
+      <h2>Marvel Characters</h2>
+      <CharactersList characters={firstFourCharacters} />
+    </div>
+  );
+}
+
+export default CharactersPage;
+
